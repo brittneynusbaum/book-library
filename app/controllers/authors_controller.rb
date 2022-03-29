5e06ffcb0a9fb43aaa9a572cc3732490e9883fce
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-28 22:39:00 -0700

    updates the author create action to take params

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,11 @@
 class AuthorsController < ApplicationController
   def create
-   
+    author = Author.new(
+      name: params[:name], 
+      age: params[:age], 
+      genre: params[:genre]
+    )
+    author.save
+    render json: author.as_json
+  end
 end
