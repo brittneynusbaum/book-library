90dcd283e3683d088cab9d80fd1f18151fb3bd7d
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-30 23:39:20 -0700

    adds create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
 
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+
+    user.save
+    render json: user.as_json
+  end
   
 end
